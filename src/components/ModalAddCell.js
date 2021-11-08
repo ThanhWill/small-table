389560d3b2b32a36c9c6f697d62c8ea09a76a909
unknown <thanh029023@gmail.com>
2021-11-08 10:20:20 +0700

    wip(Add-cell): fix add cell and fix add date

--- src/components/ModalAddCell.js
+++ src/components/ModalAddCell.js
@@ -1,59 +1,79 @@
-import { Button, Modal } from "react-bootstrap";
-import React, { useState } from "react";
-const ModalAddCell = () => {
-  const handleInsertButtonClick = (onClick) => {
-    onClick();
-  };
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
+import {Modal} from "react-bootstrap";
+import React, {useState} from "react";
+import Button from "@restart/ui/esm/Button";
+import logoClose from "../assets/close.png";
+import {useForm} from "react-hook-form";
+import PropTypes from 'prop-types';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
-  <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
-    <Modal.Header closeButton>
-      <Modal.Title>Insert Data</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      <form>
-        <div class="form-group mb-3">
-          <label for="description" className="mb-2">
-            Description
-          </label>
-          <input
-            type="text"
-            name="description"
-            class="form-control"
-            id="description"
-            placeholder="Enter Description"
-          />
-        </div>
-        <div class="form-group mb-3">
-          <label for="invoice" className="mb-2">
-            Invoice No.
-          </label>
-          <input
-            id="invoice"
-            name="invoice"
-            type="text"
-            class="form-control"
-            placeholder="Invoice No"
-          />
-        </div>
-        <div class="form-group mb-3">
-          <label for="exampleInputPassword1" className="mb-2">
-            Amount ($).
-          </label>
-          <input
-            name="amount"
-            type="number"
-            class="form-control"
-            placeholder="Amount ($)"
-          />
-        </div>
-        <button type="submit" class="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </Modal.Body>
-  </Modal>;
+const ModalAddCell = (props) => {
+    const {register, handleSubmit} = useForm({});
+    const onSubmit = (data) => {
+        props.onAddCell(data);
+    };
+
+    return (
+        <Modal show={
+                props.show
+            }
+            backdrop="static"
+            keyboard={false}>
+            <Modal.Header>
+                <Modal.Title>Insert Data</Modal.Title>
+                <div className="w-25">
+                    <Button onClick={
+                            props.close
+                        }
+                        className="btn d-flex justify-content-end shadow-none">
+                        <img src={logoClose}
+                            alt="close-img"
+                            className="w-25"/>
+                    </Button>
+                </div>
+            </Modal.Header>
+            <Modal.Body>
+                <form onSubmit={
+                    handleSubmit(onSubmit)
+                }>
+                    <div className="form-group mb-3">
+                        <label htmlFor="date" className="mb-2">
+                            Date
+                        </label>
+                        <input type="date" name="date"  {...register('date')} className="form-control" id="date" placeholder="Enter Date"/>
+                    </div>
+                    <div className="form-group mb-3">
+                        <label htmlFor="description" className="mb-2">
+                            Description
+                        </label>
+                        <input type="text" name="description" {...register('description')} className="form-control" id="description" placeholder="Enter Description"/>
+                    </div>
+                    <div className="form-group mb-3">
+                        <label htmlFor="invoice" className="mb-2">
+                            Invoice No.
+                        </label>
+                        <input id="invoice" name="invoice" type="text" {...register('invoice')} className="form-control" placeholder="Invoice No"/>
+                    </div>
+                    <div className="form-group mb-3">
+                        <label htmlFor="amount" className="mb-2">
+                            Amount ($).
+                        </label>
+                        <input name="amount" type="number" className="form-control" placeholder="Amount ($)" {...register('amount', {
+                                                                                                    valueAsNumber: true,
+                                                                                                  })}/>
+                    </div>
+                    <button type="submit" className="btn btn-primary"
+                        onClick={
+                            props.close
+                    }>
+                        Submit
+                    </button>
+                </form>
+            </Modal.Body>
+        </Modal>
+    )
 };
-
+ModalAddCell.propTypes = {
+    onAddCell: PropTypes.func
+}
 export default ModalAddCell;
--- src/components/TableData.js
+++ src/components/TableData.js
@@ -1,61 +1,117 @@
-import React, { useState } from "react";
-import { data } from "../data";
-import cellEditFactory from "react-bootstrap-table2-editor";
+import React, {useState, useMemo} from "react";
+import {data} from "../data";
+import cellEditFactory, {Type} from "react-bootstrap-table2-editor";
 import BootstrapTable from "react-bootstrap-table-next";
-
+import ModalAddCell from "./ModalAddCell"
 const TableData = () => {
-  const [show, setShow] = useState(false);
-  const handleShow = () => setShow(true);
-  const [products, setProducts] = useState(data);
-  const [columns, setColumns] = useState([
-    { dataField: "id", text: "No.", footer: "" },
-    { dataField: "date", text: "Date", footer: "" },
-    { dataField: "description", text: "Description", footer: "" },
-    { dataField: "invoice", text: "Invoice No.", footer: "Total" },
-    {
-      dataField: "amount",
-      text: "Amount ($)",
-      footer: (columnData) => columnData.reduce((acc, item) => acc + item, 0),
-    },
-    {
-      dataField: "id",
-      formatter: (cellContent, row) => {
-        return (
-          <button
-            className="btn btn-danger"
-            onClick={(e) => {
-              setProducts((_products) =>
-                _products.filter((p) => p.id !== row.id)
-              );
-            }}
-          >
-            Delete
-          </button>
-        );
-      },
-    },
-  ]);
-  return (
-    <div className="card main-panel">
-      <div className="card-body">
-        <h5>React Data</h5>
-        <div className="container-fluid">
-          <div className="d-flex">
-            <button className="btn btn-success mb-2" onClick={handleShow}>
-              Insert
-            </button>
-          </div>
-          <div className="row">
-            <BootstrapTable
-              keyField="id"
-              data={products}
-              columns={columns}
-              cellEdit={cellEditFactory({ mode: "click", blurToSave: true })}
-            />
-          </div>
+    const [show, setShow] = useState(false);
+    const handleShow = () => setShow((show) => show = !show);
+    const [products, setProducts] = useState(data);
+    const [columns, setColumns] = useState([
+        {
+            dataField: "id",
+            text: "No.",
+            footer: ""
+        },
+        {
+            dataField: "date",
+            text: "Date",
+            footer: "",
+            formatter: (cell) => {
+                let dateObj = cell;
+                if (typeof cell !== 'object') {
+                    dateObj = new Date(cell);
+                }
+                return `${
+                    ('0' + dateObj.getUTCDate()).slice(-2)
+                }/${
+                    ('0' + (
+                        dateObj.getUTCMonth() + 1
+                    )).slice(-2)
+                }/${
+                    dateObj.getUTCFullYear()
+                }`;
+            },
+            editor: {
+                type: Type.DATE
+            }
+        },
+        {
+            dataField: "description",
+            text: "Description",
+            footer: ""
+        },
+        {
+            dataField: "invoice",
+            text: "Invoice No.",
+            footer: "Total"
+        }, {
+            dataField: "amount",
+            text: "Amount ($)",
+            footer: (columnData) => columnData.reduce(
+                (acc, item) => acc + item,
+                0
+            )
+        }, {
+            dataField: "",
+            text: "",
+            formatter: (cellContent, row) => {
+                return (
+                    <button className="btn btn-danger"
+                        onClick={
+                            (e) => {
+                                setProducts((_products) => _products.filter((p) => p.id !== row.id));
+                            }
+                    }>
+                        Delete
+                    </button>
+                );
+            },
+            editable: false
+        },
+    ]);
+
+    const handerAddCell = (newData) => {
+        console.log(newData)
+        setProducts(preData => {
+            return [
+                ...preData,
+                newData
+            ]
+        })
+    }
+    useMemo(() => {
+        return products.map((p, index) => {
+            p.id = index
+        })
+    }, [products])
+    return (
+        <div className="card main-panel">
+            <div className="card-body">
+                <h5>React Data</h5>
+                <div className="container-fluid">
+                    <div className="d-flex">
+                        <button className="btn btn-success mb-2"
+                            onClick={handleShow}>
+                            Insert
+                        </button>
+                    </div>
+                    <div className="row">
+                        <BootstrapTable keyField="id"
+                            data={products}
+                            columns={columns}
+                            cellEdit={
+                                cellEditFactory({mode: "click", blurToSave: true})
+                            }/>
+                        <ModalAddCell show={show}
+                            close={
+                                () => setShow(false)
+                            }
+                            onAddCell={handerAddCell}/>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 export default TableData;
